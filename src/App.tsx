365a64d19c78aa4e0c1106c188451f11ccbe4079
Nikki Odd <archiektor@gmail.com>
2023-09-16 11:11:31 +0200

    - ended 13pt.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import BackgroundImage from './img/gregory-gomez-OqgeSOpM1UE-unsplash.jpg';
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
