4e895d150cc511a406846a12dd756bc2fc289672
Nikki Odd <archiektor@gmail.com>
2023-09-19 08:06:59 +0200

    - rebuilt structure

--- src/components/Profile/Post/Post.tsx
+++ src/components/Profile/Post/Post.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import './Post.css';
 
 type PostPropsType = {
-    message: string
+    message: string,
+    likesCount: number,
 }
 
 const Post = (props: PostPropsType) => {
-    const {message} = props;
+    const {message, likesCount} = props;
 
     return (
         <div className={`post`}>
-            <p className={`post__text`}>{message}</p>
+            <p className={`post__text`}>{message} </p>
+            <span className={`post__like`}>{likesCount} likes</span>
         </div>
     )
 }
