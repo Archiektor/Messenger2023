493d464f63e85a1c52309e98273032eea3eea4cb
Nikki Odd <archiektor@gmail.com>
2023-10-05 20:00:39 +0200

    - ended 31pt.

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -9,13 +9,24 @@ type MyPostsType = {
 
 const MyPosts: React.FC<MyPostsType> = ({posts}) => {
 
+    const newPostElement = React.createRef<HTMLInputElement>();
+
+    const addPost = () => {
+        let text = '';
+        if (newPostElement.current) {
+            text = newPostElement.current.value;
+        }
+        alert(text);
+    }
+
     return (
         <div className={'posts-wrapper'}>
             <div className={'posts'}>
                 <div className={`new-post`}>
                     <h3 className={`new-post__title`}>My posts</h3>
-                    <input className={`new-post__write-text`} type="text" placeholder={`your news`}/>
-                    <button className={`new-post__send-btn`}>Send</button>
+                    <input ref={newPostElement} className={`new-post__write-text`} type="text"
+                           placeholder={`your news`}/>
+                    <button onClick={() => addPost()} className={`new-post__send-btn`}>Send</button>
                 </div>
             </div>
             <div className={'comments'}>
